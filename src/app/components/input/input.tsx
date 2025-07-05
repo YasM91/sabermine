195ca18a854af879d00915ca1835223e0805ad98
Yaseen <yaseenm990@gmail.com>
2025-07-05 19:15:23 +0100

    add error message

--- src/app/components/input/input.tsx
+++ src/app/components/input/input.tsx
@@ -3,12 +3,18 @@ import styles from './input.module.css';
 
 type Props = InputHTMLAttributes<HTMLInputElement> & {
   label?: string;
+  error?: boolean;
 };
 
-const Input = ({ label, id, ...props }: Props) => (
+const Input = ({ label, id, error = false, ...props }: Props) => (
   <div>
     {label && <label htmlFor={id}>{label}</label>}
     <input id={id} className={styles.input} {...props} />
+    {error && (
+      <span className={styles.error} aria-live="polite" role="status">
+        Invalid regex, please try again
+      </span>
+    )}
   </div>
 );
 
